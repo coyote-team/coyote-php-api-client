760bc6709f1d65de4e2e77996d5bd3ff1e79b443
Job van Achterberg <mail@3ode.nl>
2022-05-16 16:06:20 +0200

    Include OrganizationId in URI when INCLUDE_ORG_ID is set

--- src/InternalApiClient.php
+++ src/InternalApiClient.php
@@ -41,12 +41,8 @@ class InternalApiClient
      */
     public function get(string $url, array $options = []): ?stdClass
     {
-        $includeOrganizationId = array_key_exists(self::INCLUDE_ORG_ID, $options)
-            ? $options[self::INCLUDE_ORG_ID]
-            : false;
-
         return self::request(
-            $this->makeUrl($url, $includeOrganizationId),
+            $this->makeUrl($url, $options),
             array_merge($options, ['method' => self::METHOD_GET])
         );
     }
@@ -62,7 +58,7 @@ class InternalApiClient
     public function post(string $url, array $payload, array $options = []): ?stdClass
     {
         return self::request(
-            $this->makeUrl($url),
+            $this->makeUrl($url, $options),
             array_merge($options, ['method' => self::METHOD_POST], ['json' => $payload])
         );
     }
@@ -78,13 +74,17 @@ class InternalApiClient
     public function put(string $url, array $payload, array $options = []): ?stdClass
     {
         return self::request(
-            $this->makeUrl($url),
+            $this->makeUrl($url, $options),
             array_merge($options, ['method' => self::METHOD_PUT], ['json' => $payload])
         );
     }
 
-    private function makeUrl(string $part, bool $includeOrganizationId = false): string
+    private function makeUrl(string $part, array $options): string
     {
+        $includeOrganizationId = array_key_exists(self::INCLUDE_ORG_ID, $options)
+            ? $options[self::INCLUDE_ORG_ID]
+            : false;
+
         return $includeOrganizationId
             ? sprintf('%s/organizations/%d/%s', $this->endpoint, $this->organizationId, $part)
             : sprintf('%s/%s', $this->endpoint, $part);
