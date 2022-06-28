35320a3579bb43397c00646f4971bc59ced19501
Job van Achterberg <mail@3ode.nl>
2022-06-28 16:17:30 +0200

    Remove deprecated dependencies from ResourceUpdatePayloadApiModel

--- src/ApiPayload/ResourceUpdatePayloadApiModel.php
+++ src/ApiPayload/ResourceUpdatePayloadApiModel.php
@@ -3,8 +3,6 @@
 namespace Coyote\ApiPayload;
 
 use Coyote\ApiModel\OrganizationApiModel;
-use Coyote\ApiModel\Partial\ApiMetaData;
-use Coyote\ApiModel\Partial\ResourceLinks;
 use Coyote\ApiModel\ResourceApiModel;
 use Coyote\ApiModel\ResourceRepresentationApiModel;
 
